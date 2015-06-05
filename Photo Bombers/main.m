1d97d0bc8186c03cb266e4681c15565047916ec5
Sunshine Yang <sunshineyang@Sunshines-MacBook-Pro.local>
2015-06-05 12:33:06 +0200

    Initial Commit

---
+++ Photo Bombers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Sunshine Yang on 6/5/15.
+//  Copyright (c) 2015 SunshineYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
